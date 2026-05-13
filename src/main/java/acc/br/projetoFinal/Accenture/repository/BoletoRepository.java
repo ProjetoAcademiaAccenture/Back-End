4a7cf16a891579234b96a0b307b83b4f417493d3
Fernando Sena Cruz <fernandosenacruz@gmail.com>
2026-05-13 08:35:31 -0300

    feat: Refactor Boleto to associate with Pagamento, enhance payment validation, and update response DTO

--- src/main/java/acc/br/projetoFinal/Accenture/repository/BoletoRepository.java
+++ src/main/java/acc/br/projetoFinal/Accenture/repository/BoletoRepository.java
@@ -3,10 +3,11 @@ package acc.br.projetoFinal.Accenture.repository;
 import acc.br.projetoFinal.Accenture.model.Boleto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface BoletoRepository extends JpaRepository<Boleto, Long> {
     Optional<Boleto> findByCodigoBarras(String codigoBarras);
-    Optional<Boleto> findByPedidoId(Long pedidoId);
+    Optional<Boleto> findByPagamentoId(Long pagamentoId);
 }
