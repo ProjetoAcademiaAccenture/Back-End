40d258e04c578f23bd59ca79b9ebaaf651136cd9
HenriqueFMA <carolinahentiquef@gmail.com>
2026-05-15 19:19:34 -0300

    feat: add Categoria to produtoRequest in SystemIntegrationTests for enhanced product categorization

--- src/test/java/acc/br/projetoFinal/Accenture/service/SystemIntegrationTests.java
+++ src/test/java/acc/br/projetoFinal/Accenture/service/SystemIntegrationTests.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
+import acc.br.projetoFinal.Accenture.enums.Categoria;
 /**
  * Testes de integração do sistema Loja + Banco
  * Cobre: Cliente, Conta, Produto, Depósito e validações
@@ -89,6 +89,8 @@ class SystemIntegrationTests {
         produtoRequest.setDescricao("Mouse Óptico");
         produtoRequest.setPreco(new BigDecimal("100.00"));
         produtoRequest.setQuantidadeEstoque(5);
+        produtoRequest.setCategoria(acc.br.projetoFinal.Accenture.enums.Categoria.ELETRONICOS); 
+
 
         produto = produtoService.criar(produtoRequest);
     }
